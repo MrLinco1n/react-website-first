0a140eb63b866d7720ce65c1e93c599ba18bd8e5
MrLinco1n <jaykmcwhorter@hotmail.com>
2021-03-07 21:29:33 -0700

    init

---
+++ src/components/HeroSection.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+import video from "../videos/video-2.mp4" //THIS BIT
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <video src={video} autoPlay loop muted />
+      <h1>Nature awaits</h1>
+      <p>What are you waiting for?</p>
+      <div className='hero-btns'>
+      <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          Learn more
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          WATCH VIDEO <i className='far fa-play-circle' />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
